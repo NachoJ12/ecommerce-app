ec9bac8c147d0c8be6684c6c560de928b0765020
NachoJ12 <nacho.justel12@gmail.com>
2022-05-27 19:25:39 -0300

    resuelvo desafio 2 - component navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import './NavBar.css';
+import logo from '../logo.svg';
+
+const NavBar = () => {
+  return (
+    <nav className="navBar">
+      <div className="brand">
+        <img src={logo} alt="logo" />
+        <h2 className="nameEcommerce">Electro App</h2>
+      </div>
+      <ul>
+        <li>
+          <a href="#electrodomesticos">Electrodomésticos</a>
+        </li>
+        <li>
+          <a href="#informatica">Informática</a>
+        </li>
+        <li>
+          <a href="#celulares">Celulares</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
